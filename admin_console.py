ee3eb9dd1843e0194b07e3f612ec356fdb2bb800
NobWow <pashok15789@gmail.com>
2021-11-17 21:40:02 +0600

    Docstrings

--- admin_console.py
+++ admin_console.py
@@ -1,3 +1,22 @@
+"""
+Embeddable asynchronous console command prompt with extension support
+Example:
+
+import asyncio
+from admin_console import AdminCommandExecutor, basic_command_set
+
+async def main():
+    ace = AdminCommandExecutor()
+    basic_command_set(ace)
+    await ace.load_extensions()
+    print("Terminal. Type help to see the list of commands")
+    await ace.prompt_loop()
+    print("Exit from command prompt")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+"""
 import asyncio
 import traceback
 from ainput import AsyncRawInput, colors
@@ -21,6 +40,7 @@ def str_findall_unescaped(str_: str, char: int):
 
 
 def parse_escapes(inp: str):
+    """Convert hexadecimal escape code sequences \\xNN into actual symbols"""
     res = ''
     _last = -4
     for i in range(inp.count('\\x') + 1):
@@ -41,7 +61,8 @@ def parse_escapes(inp: str):
         return inp
 
 
-def validate_convert_args(argtypes: list, args: list):  # return list if arguments are valid, int if error (index of invalid arg)
+def validate_convert_args(argtypes: tuple, args: list):  # return list if arguments are valid, int if error (index of invalid arg)
+    """Validate and cast string variables into their specified type from argtypes"""
     cargs = []
     for i in range(len(args)):
         assert type(args[i]) is str, 'didn\'t you forget that you pass arguments as str because its input from the console? You just try to convert it to the type of arg...'
@@ -52,6 +73,21 @@ def validate_convert_args(argtypes: list, args: list):  # return list if argumen
 
 
 def paginate_list(list_: list, elemperpage: int, cpage: int) -> (int, list):
+    """Extract a page from a list
+    Parameters
+    ----------
+    list_ : list
+        List of the elements
+    elemperpage : int
+        Amount of elements per page
+    cpage : int
+        Number of selected page
+
+    Returns
+    -------
+    int, list
+        amount of pages
+        the list page"""
     maxpages = ceil(len(list_) / elemperpage)
     cpage = max(1, min(cpage, maxpages))
     start_at = max((cpage - 1) * elemperpage, 0)
@@ -60,6 +96,22 @@ def paginate_list(list_: list, elemperpage: int, cpage: int) -> (int, list):
 
 
 def paginate_range(count: int, elemperpage: int, cpage: int) -> (int, int, int):
+    """Similar to the paginate_list(list, elemperpage, cpage), but creates a range instead of the list
+    Parameters
+    ----------
+    count : int
+        Amount of all elements
+    elemperpage : int
+        Amount of elements per page
+    cpage : int
+        Number of selected page
+
+    Returns
+    -------
+    int, int, int
+        amount of pages
+        ID of the first element in the page
+        ID of the last element in the page"""
     maxpages = ceil(count / elemperpage)
     cpage = max(1, min(cpage, maxpages))
     start_at = max((cpage - 1) * elemperpage, 0)
@@ -70,6 +122,36 @@ def paginate_range(count: int, elemperpage: int, cpage: int) -> (int, int, int):
 class AdminCommand():
 
     def __init__(self, afunc, name: str, args: list, optargs: list, description: str = ''):
+        """Represents a console command.
+        To add a command of an extension, use AdminCommandExtension.add_command(
+            afunc, name, args, optargs, description) instead
+        Parameters
+        ----------
+        afunc : coroutine
+            await afunc(AdminCommandExecutor, *args)
+            Coroutine function that represents the command functor and receives parsed arguments
+        name : str
+            Name of the command
+        args : list
+            [(type : class, name : str), ...]
+            List of the mandatory arguments of the command
+
+            Possible types are: str, int, float, bool, None
+            -------------------
+            str: a word or a "string"
+            int: valid number, 123
+            float: a number with floating point, 123.456
+            bool: switch argument, can be yes / no, true / false, y / n
+            None: raw string, such an argument could be only the last argument of the command
+                Cannot be used with at least 1 argument from optargs
+            -------------------
+        optargs : list
+            [(type : class, name : str), ...]
+            List of the optional arguments of the command
+            Types are described in args
+        description : str
+            Description of what this command does. Shows in help
+        """
         self.name = name
         self.args = args  # tuple (type, name)
         self.optargs = optargs  # tuple (type, name)
@@ -79,11 +161,29 @@ class AdminCommand():
         self.afunc = afunc  # takes AdminCommandExecutor and custom args
 
     async def execute(self, executor, args):
+        """Shouldn't be overriden, use afunc to assign a functor to the command"""
         await self.afunc(executor, *args)
 
 
 class AdminCommandExtension():
     def __init__(self, ACE, name: str, module: types.ModuleType, logger: logging.Logger = logging.getLogger('main')):
+        """Extension data class. Constructed by AdminCommandExecutor.
+        In extension scripts the instance is passed into:
+            async def extension_init(AdminCommandExtension)
+                called when an extension is loaded
+            async def extension_cleanup(AdminCommandExtension)
+                called when an extension is unloaded
+        Parameters
+        ----------
+        ACE : AdminCommandExecutor
+            instance of the AdminCommandExecutor class holding this extension.
+        name : str
+            name of the extension or extensionless filename of the extension
+        module : types.ModuleType
+            Python importlib module of the extension
+        logger : logging.Logger
+            logging.Logger instance attached to an extension
+        """
         self.ace = ACE
         self.tasks = {
             # 'task_name': asyncio.Task
@@ -97,23 +197,65 @@ class AdminCommandExtension():
         self.logger = logger
 
     def sync_local_commands(self, overwrite=False):
+        """Adds all the extension commands into AdminCommandExecutor commands list
+        Parameters
+        ----------
+        overwrite : bool
+            If True, already existing commands will be replaced
+            If False, fails in case when any of the extension commands are overlapping with already existing.
+
+        Returns
+        -------
+        bool
+            Success
+        """
         if not self.commands:
             return False
         if overwrite:
-            self.commands.update(self.commands)
+            self.ace.commands.update(self.commands)
             return True
         if self.commands.keys().isdisjoint(self.ace.commands.keys()):
             self.ace.commands.update(self.commands)
-
-    def add_command(self, afunc, name: str, args: list, optargs: list, description: str = '', replace=False):
-        if name in self.ace.commands:
+            return True
+        else:
             return False
+
+    def add_command(self, afunc, name: str, args: list, optargs: list = [], description: str = '', replace=False):
+        """Registers a command and adds it to the AdminCommandExecutor.
+        Constructs an AdminCommand instance with all the arguments passed.
+        Doesn't require sync_local_commands() to be run
+        Parameters
+        ----------
+        see AdminCommand
+        replace : bool
+            Whether or not the command should be replaced if already exists. Defaults to False
+
+        Returns
+        -------
+        bool
+            Success
+        """
         cmd = AdminCommand(afunc, name, args, optargs, description)
-        self.commands[name] = cmd
-        self.ace.commands[name] = cmd
-        return True
+        if name not in self.ace.commands or (name in self.ace.commands and replace):
+            self.commands[name] = cmd
+            self.ace.commands[name] = cmd
+            return True
+        else:
+            return False
 
     def remove_command(self, name: str, remove_native=False):
+        """Unregisters a command from the AdminCommandExtension and/or from an AdminCommandExecutor
+        If remove_native is True, it doesn't check whether or not this command is owned by this extension
+        Parameters
+        ----------
+        remove_native : bool
+            If False, delete only if this command is owned by self
+
+        Returns
+        -------
+        bool
+            Success
+        """
         if remove_native:
             if name in self.ace.commands:
                 del self.ace.commands[name]
@@ -130,6 +272,13 @@ class AdminCommandExtension():
             return True
 
     def clear_commands(self):
+        """Clear all the commands registered by this extension
+
+        Returns
+        -------
+        bool
+            Success
+        """
         for cmdname in self.commands:
             if cmdname in self.ace.commands:
                 del self.ace.commands[cmdname]
@@ -138,14 +287,87 @@ class AdminCommandExtension():
         return True
 
     def msg(self, msg):
+        """Show message in the console with the extension prefix"""
         self.ace.print('[%s] %s' % (self.name, msg))
 
     def logmsg(self, msg: str, level: int = logging.INFO):
+        """Write a message into the log"""
         self.logger.log(1, '[%s] %s' % (self.name, msg))
 
 
 class AdminCommandExecutor():
-    def __init__(self, stuff: dict, use_config=True, logger: logging.Logger = None, extension_path='extensions/'):
+    """This is the main class of the library. It handles command execution and extension load/unload
+    Firstly, an extensions should be loaded before prompting for commands.
+
+    ace = AdminCommandExecutor()
+    await ace.load_extensions()
+
+    Launching a prompt. Waits until exit command is invoked.
+    await ace.prompt_loop()
+
+    Perform a cleanup
+    await ace.full_cleanup()
+
+    Members
+    -------
+    AdminCommandExecutor.stuff : dict
+        Arbitrary data storage. Can be used to share data between extensions
+    AdminCommandExecutor.commands : dict
+        dictionary of a commands
+            {"name": AdminCommand}
+    AdminCommandExecutor.lang : dict
+        dictionary of formattable strings
+        {
+            'nocmd': '%s: unknown command',
+            'usage': 'Usage: %s',
+            'invalidarg': '%s is invalid, check your command arguments.',
+            'toomanyargs': 'warning: this command receives %s arguments, you provided %s or more',
+            'notenoughargs': 'not enough arguments: the command receives %s arguments, you provided %s.'
+        }
+    AdminCommandExecutor.types : dict
+        dictionary of typenames
+        {
+            str: 'word',
+            int: 'n',
+            float: 'n.n',
+            bool: 'yes / no',
+            None: 'text...'
+        }
+    AdminCommandExecutor.extpath : str
+    self.prompt_dispatching : bool = True
+        Set this variable to False to stop prompt_loop() after a command dispatch
+    self.promptheader : str
+        Command prompt header. Defaults to 'nothing'
+    self.promptarrow : str
+        Command prompt arrow. Defaults to '>'
+    Command prompt header and arrow are combined into a prompt
+    self.history : list
+        Last executed commands, can be cycled with an arrow keys.
+    self.ainput : AsyncRawInput(history=self.history)
+        AsyncRawInput class for reading user input in raw tty mode
+    self.ainput.ctrl_c = self.full_cleanup
+        Async function for handling Ctrl + C
+    self.prompt_format = {'bold': True, 'fgcolor': colors.GREEN}
+        Formatting of the prompt header and arrow.
+    self.input_format = {'fgcolor': 10}
+        Formatting of the user input in terminal
+    Others:
+    self.print = self.ainput.writeln
+    self.logger = logger
+    """
+    def __init__(self, stuff: dict = {}, use_config=True, logger: logging.Logger = None, extension_path='extensions/'):
+        """
+        Parameters
+        ----------
+        stuff : dict
+            Arbitrary data storage
+        use_config : bool
+            Whether or not a config.json file should be created or loaded in a working directory. Defaults to True
+        logger : logging.Logger
+            logging.Logger instance to handle all the log messages
+        extension_path : str
+            Relative or absolute path to the extensions directory. Defaults to "extensions/"
+        """
         self.stuff = stuff
         self.use_config = use_config
         self.commands = {}
@@ -186,30 +408,54 @@ class AdminCommandExecutor():
         if use_config:
             self.load_config()
 
-    def error(self, msg):
+    def error(self, msg: str):
+        """Shows a red error message in the console and logs.
+        ERROR: msg
+
+        Parameters
+        ----------
+        msg : str
+            Message"""
         if self.logger:
             self.logger.error(msg)
         else:
             self.ainput.writeln('ERROR: %s' % msg, fgcolor=colors.RED)
 
-    def info(self, msg):
+    def info(self, msg: str):
+        """Shows a regular info message in the console and logs.
+
+        Parameters
+        ----------
+        msg : str
+            Message
+        """
         if self.logger:
             self.logger.info(msg)
         else:
             self.ainput.writeln('INFO: %s' % msg)
 
-    def log(self, msg, level=10):
+    def log(self, msg: str, level=10):
+        """Shows a log message in the console and logs
+
+        Parameters
+        ----------
+        msg : str
+            Message
+        level : int
+            Level of the log message between 0 and 50
+        """
         if self.logger:
             self.logger.log(level, msg)
-        else:
+        elif level >= self.logger.getEffectiveLevel():
             levels = {0: 'NOTSET', 10: 'DEBUG', 20: 'INFO', 30: 'WARNING', 40: 'ERROR', 50: 'CRITICAL'}
             self.ainput.writeln('%s: %s' % (levels[level], msg))
 
     async def load_extensions(self):
-        extlist = []
+        """Loads extensions from an extension directory specified in AdminCommandExecutor.extpath"""
+        extlist = set()
         if os.path.exists(os.path.join(self.extpath, 'extdep.txt')):
             with open(os.path.join(self.extpath, 'extdep.txt'), 'r') as f:
-                extlist.extend('%s.py' % x for x in f.readlines())
+                extlist.update('%s.py' % x for x in f.readlines())
         else:
             self.print('Note: create extdep.txt in the extensions folder to sequentally load modules')
         if not os.path.exists(self.extpath):
@@ -221,13 +467,25 @@ class AdminCommandExecutor():
         with os.scandir(self.extpath) as extpath:
             for file in extpath:
                 if file.name.endswith('.py') and file.is_file() and file.name not in extlist:
-                    extlist.append(file.name)
+                    extlist.add(file.name)
         for name in extlist:
             if not os.path.exists(os.path.join(self.extpath, name)):
                 self.error('Module file %s not found' % name)
             await self.load_extension(name.split('.')[0])
 
     def load_config(self, path: str = 'config.json'):
+        """Loads a configuration from a JSON file
+
+        Parameters
+        ----------
+        path : str
+            Absolute or relative path to the config file. Defaults to "config.json"
+
+        Returns
+        -------
+        bool
+            Success
+        """
         try:
             file = open(path, 'r')
             self.info('')
@@ -242,6 +500,18 @@ class AdminCommandExecutor():
             return False
 
     def save_config(self, path: str = 'config.json'):
+        """Saves a configuration into a JSON file
+
+        Parameters
+        ----------
+        path : str
+            Absolute or relative path to the config file. Defaults to "config.json"
+
+        Returns
+        -------
+        bool
+            Success
+        """
         try:
             file = open(path, 'w')
             self.config = json.dumps(self.config, indent=True)
@@ -249,8 +519,21 @@ class AdminCommandExecutor():
             return True
         except OSError:
             self.error('Failed to save configuration at %s' % path)
+            return False
 
     async def load_extension(self, name: str):
+        """Loads a Python script as an extension from its extension path and call await extension_init(AdminCommandExtension)
+
+        Parameters
+        ----------
+        name : str
+            The extensionless name of a file containing a Python script
+
+        Returns
+        -------
+        bool
+            Success
+        """
         spec = importlib.util.spec_from_file_location(name, os.path.join(self.extpath, name + '.py'))
         module = importlib.util.module_from_spec(spec)
         try:
@@ -268,6 +551,19 @@ class AdminCommandExecutor():
             return False
 
     async def unload_extension(self, name: str, keep_dict=False):
+        """Unload an extension and call await extension_cleanup(AdminCommandExtension)
+        Parameters
+        ----------
+        name : str
+            The name of an extension
+        keep_dict : bool
+            Whether or not this extension should be kept in the list. Defaults to False
+
+        Returns
+        -------
+        bool
+            Success
+        """
         if name not in self.extensions:
             return None
         for task in self.extensions[name].tasks:
@@ -284,6 +580,18 @@ class AdminCommandExecutor():
             return False
 
     async def dispatch(self, cmdline: str):
+        """Executes a command. Shouldn't be used explicitly. Use prompt_loop() instead.
+
+        Parameters
+        ----------
+        cmdline : str
+            A whole line representing a command
+
+        Returns
+        -------
+        bool
+            Success
+        """
         args = []
         cmdname, _, argl = cmdline.partition(' ')
         if cmdname not in self.commands:
@@ -342,6 +650,21 @@ class AdminCommandExecutor():
             self.error("Command execution failed: %s" % exc)
 
     def usage(self, cmdname: str, lang=True):
+        """Get a formatted usage string for the command
+        Raises KeyError if the command doesn't exist.
+
+        Parameters
+        ----------
+        cmdname : str
+            Name of the command
+        lang : bool
+            Whether or not returned string should be formatted through lang['usage']
+
+        Returns
+        -------
+        str
+            Formatted string representing a usage of the command
+        """
         cmd = self.commands[cmdname]
         if cmd.args:
             mandatory_args = ['<%s: %s>' % (x[1], self.types[x[0]]) for x in cmd.args]
@@ -358,6 +681,10 @@ class AdminCommandExecutor():
             return usage
 
     async def prompt_loop(self):
+        """Asynchronously prompt for commands and dispatch them
+        Blocks asynchronously until the prompt is closed.
+        Prefer launching in asyncio.Task wrapper.
+        """
         self.ainput.prepare()
         # loop = asyncio.get_event_loop()
         # loop.add_signal_handler(2, self.ctrl_c)
@@ -376,6 +703,8 @@ class AdminCommandExecutor():
         self.print('Exit from command prompt.')
 
     async def full_cleanup(self):
+        """Perform cleanup steps in AdminCommandExecutor.full_cleanup_steps
+        """
         self.prompt_dispatching = False
         self.ainput.is_reading = False
         for func in self.full_cleanup_steps:
